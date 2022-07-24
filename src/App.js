89ba827612292ec4b0c5b49b6469d2be6cdcb14c
Ram kumar <webbieram@outlook.com>
2022-07-24 20:36:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Title from './components/Title';
+import FirstRequest from './examples/1-first-request';
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <FirstRequest/>
+    </main>
+  );
+}
+
+export default App;
